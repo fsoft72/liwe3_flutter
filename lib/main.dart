fb007b8c9c716cb369a0cdd64857402b13a5a7d3
fsoft <fabio.rotondo@gmail.com>
2024-02-21 08:44:08 +0100

    Refactor InputText and add TagInput component

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:liwe3_flutter/liwe3/components/button.dart';
 import 'package:liwe3_flutter/liwe3/components/input_text.dart';
+import 'package:liwe3_flutter/liwe3/components/tag_input.dart';
 
 void main() => runApp(const MyApp());
 
@@ -47,36 +48,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -85,16 +63,14 @@ class _MyHomePageState extends State<MyHomePage> {
               obscureText: false,
               placeholder: "Type something",
             ),
-            Button(
+            const Button(
               label: "Click me",
               backgroundColor: Colors.red,
               color: Colors.white,
               borderRadius: 10,
               borderColor: Colors.black,
-              onPressed: () {
-                _incrementCounter();
-              },
             ),
+            TagInput(),
           ],
         ),
       ),
