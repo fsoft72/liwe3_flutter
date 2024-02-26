b62b1bca4556d5084df58d8061af08d111e5dd08
fsoft <fabio.rotondo@gmail.com>
2024-02-26 10:46:18 +0100

    0.0.2 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+# 0.0.2
+
+- A lot of work on InputText field to add support for `type`, validChaaracters, right keyboard layout, and more.
+
 # 0.0.1
 
 - Initial release
--- scripts/flutter_publish.sh
+++ scripts/flutter_publish.sh
@@ -21,18 +21,18 @@ if [ $? -ne 0 ]; then
 fi
 
 # Update the version in the pubspec.yaml file
-version=$(grep 'version: ' pubspec.yaml | sed 's/version: //')
-echo "Current version: $version"
+#version=$(grep 'version: ' pubspec.yaml | sed 's/version: //')
+#echo "Current version: $version"
 
 # Updates the last number of the version
-new_version=$(echo $version | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
-echo "New version: $new_version"
+#new_version=$(echo $version | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
+#echo "New version: $new_version"
 
 # Update the version in the pubspec.yaml file
-sed -i '' "s/^version: .*/version: $new_version/g" pubspec.yaml
+#sed -i "s/^version: .*/version: $new_version/g" pubspec.yaml
 
 # Commit the changes
-git add pubspec.yaml
-git commit -m "Published version $version"
+#git add pubspec.yaml
+#git commit -m "Published version $version"
 
 flutter pub publish
