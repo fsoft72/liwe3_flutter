4ee114829ee6405a73cb016604c79dc302bcff9c
fsoft <fabio.rotondo@gmail.com>
2024-02-26 13:10:49 +0100

    Add AppStore class to manage app state

---
+++ lib/stores/app.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class AppStore extends GetxController {
+  String baseURL = '';
+  String token = '';
+  String refreshToken = '';
+}
+
+final AppStore appStore = AppStore();
