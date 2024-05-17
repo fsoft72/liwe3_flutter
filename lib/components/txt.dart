f2ad265df5e473fd1b33741e269074236e90e9d8
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-05-17 08:31:06 +0200

    chore: Remove unused import in txt.dart

--- lib/components/txt.dart
+++ lib/components/txt.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: no_leading_underscores_for_local_identifiers
 
 import 'package:flutter/material.dart';
-import 'package:liwe3/utils/debug.dart';
 
 import '../utils/intenals.dart';
 
