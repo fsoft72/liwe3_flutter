df47e4bffdb81a333cd923602afb8d947ea4c6cd
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-05-17 08:05:03 +0200

    ADD: gradient support to Txt()

--- lib/components/txt.dart
+++ lib/components/txt.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers
+
 import 'package:flutter/material.dart';
+import 'package:liwe3/utils/debug.dart';
 
 class Txt extends StatelessWidget {
   final String txt;
@@ -9,6 +12,7 @@ class Txt extends StatelessWidget {
   final String? fontFamily;
   final Color? color;
   final double? fontSize;
+  final List<Color>? gradient;
 
   const Txt(
     this.txt, {
@@ -20,20 +24,44 @@ class Txt extends StatelessWidget {
     this.fontFamily,
     this.color,
     this.fontSize,
+    this.gradient,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      txt,
-      style: TextStyle(
-        color: color,
-        fontSize: fontSize,
-        fontWeight: isBold ? FontWeight.bold : fontWeight,
-        fontStyle: isItalic ? FontStyle.italic : FontStyle.normal,
-        decoration: isUnderline ? TextDecoration.underline : TextDecoration.none,
-        fontFamily: fontFamily,
-      ),
-    );
+    Widget _mkTxt() {
+      return Text(
+        txt,
+        style: TextStyle(
+          color: color,
+          fontSize: fontSize,
+          fontWeight: isBold ? FontWeight.bold : fontWeight,
+          fontStyle: isItalic ? FontStyle.italic : FontStyle.normal,
+          decoration: isUnderline ? TextDecoration.underline : TextDecoration.none,
+          fontFamily: fontFamily,
+        ),
+      );
+    }
+
+    // if colors is not null, define stops so that the first is 0, the last is 1, and the is calculated
+    // based on the length of the colors list
+    if (gradient != null) {
+      final double _stop = 1 / gradient!.length;
+      final List<double> _stops = List<double>.generate(gradient!.length, (index) => _stop * (index + 1));
+      _stops[0] = 0;
+
+      return ShaderMask(
+        blendMode: BlendMode.srcIn,
+        shaderCallback: (bounds) => LinearGradient(
+          colors: gradient!,
+          stops: _stops,
+        ).createShader(
+          Rect.fromLTWH(10, 0, bounds.width, bounds.height),
+        ),
+        child: _mkTxt(),
+      );
+    }
+
+    return _mkTxt();
   }
 }
